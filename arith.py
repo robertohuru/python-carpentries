f91d62f4db8b9f8e0013b9b562656921c21bd5f1
Robert Ohuru <robertohuru@gmail.com>
2025-11-19 14:15:09 +0100

    gitautosync update 156

--- arith.py
+++ arith.py
@@ -8,4 +8,9 @@ def main():
 
     num1 = sys.argv[2]
     num2 = sys.arg[3]
-    
+
+    assert action in action_list, "Operation not supported " + action
+
+    if action == "--add":
+        print(float(num1) + float(num2))
+    elif action == ""
